fffa672b1429dd5cc7a65a16af53aa24fb0d8235
estebancb03 <estebanr.castaneda03@gmail.com>
2021-11-20 16:46:52 -0600

    Se crea prototipo de clase AdjacencyMatrixGraph

---
+++ Graphs/AdjacencyMatrixGraph.h
@@ -0,0 +1,12 @@
+#ifndef ADJACENCYMATRIXGRAPH_H
+#define ADJACENCYMATRIXGRAPH_H
+#include "../Arista/Arista.h"
+#include "../Vertex/Vertex.h"
+
+template < class T, class Q >
+class Graph {
+
+    public:
+};
+
+#endif //ADJACENCYMATRIXGRAPH_H
