52ea6bcab6dad045fe37059e529ca1edb64243d4
estebancb03 <estebanr.castaneda03@gmail.com>
2021-11-21 01:07:01 -0600

    Se crean metodos y propiedades de la arista

--- Arista/Arista.h
+++ Arista/Arista.h
@@ -1,10 +1,20 @@
 #ifndef ARISTA_H
 #define ARISTA_H
+#include "../Vertex/Vertex.h"
 
 template < class T >
 class Arista {
-
+    Q weight;
+    Vertex< Q > *originVertex;
+    Vertex< Q > *destinationVertex;
     public:
+        Vertex(Q w) { weight = w; originVertex = nullptr; destinationVertex = nullptr; };
+        Q getWeight() { return weight; };
+        Vertex< Q > *getOriginVertex() { return originVertex; };
+        Vertex< Q > *getDestinationVertex() { return destinationVertex; };
+        void setWeight(Q w) { weight = w; };
+        void setOriginVertex(Vertex< Q > *ov) { originVertex = ov; };
+        void setDestinationVertex(Vertex< Q > *dv) { destinationVertex = dv; };
 };
 
 #endif //ARISTA_H
