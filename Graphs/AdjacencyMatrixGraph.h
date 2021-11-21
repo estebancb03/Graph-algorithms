2dcae01a907e7a22cf21190e44e60032337e4a92
estebancb03 <estebanr.castaneda03@gmail.com>
2021-11-21 01:25:35 -0600

    Se crea metodo create de grafo con matriz de adyacencia

--- Graphs/AdjacencyMatrixGraph.h
+++ Graphs/AdjacencyMatrixGraph.h
@@ -1,12 +1,26 @@
 #ifndef ADJACENCYMATRIXGRAPH_H
 #define ADJACENCYMATRIXGRAPH_H
-#include "../Arista/Arista.h"
 #include "../Vertex/Vertex.h"
 
 template < class T, class Q >
 class Graph {
-    
+    int size;
+    Q **adjacencyMatrix;
+    Vertex< T > *vertexArray;
     public:
+        void create();
 };
+
+/*
+    EFECTO:
+    REQUIERE:
+    MODIFICA:
+*/
+template < typename T, typename Q >
+void Graph< T, Q > :: create() {
+    size = 100;
+    vertexArray = new Vertex< T >[size];
+    adjacencyMatrix = new Q[size][size];
+}
 
 #endif //ADJACENCYMATRIXGRAPH_H
