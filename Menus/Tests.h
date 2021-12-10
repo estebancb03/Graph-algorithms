0fc4a3d1e89f2a92c0e3452bf31a2a5759a2154c
estebancb03 <estebanr.castaneda03@gmail.com>
2021-12-09 22:40:07 -0600

    Metodos para testeo

--- Menus/Tests.h
+++ Menus/Tests.h
@@ -1,32 +1,112 @@
 #ifndef TESTS_H
 #define TESTS_H
+#include <chrono>
+#include <time.h>
 #include "Menu.h"
 
 template < class T, class Q >
 class Tests {
-    Graph graph;
+    Graph< T, Q > *graph;
     GenericGraphAlgorithms< T, Q > *graphAlgoritms;
     public:
         void test();
         void createBigGraph();
         void createSmallGraph();
-        void createMiddleGraph();
+        void createMediumGraph();
 };
 
+template < typename T, typename Q >
 void Tests< T, Q > :: test() {
-    
+    createBigGraph();
+    auto start = std :: chrono :: system_clock :: now();
+    graphAlgoritms -> nDijkstra();
+    auto end = std :: chrono :: system_clock :: now();
+    std :: chrono :: duration< float > enlapsed_seconds = end - start;
+    std :: cout << endl << endl << "Tiempo: " << enlapsed_seconds.count() << " segundos" << endl;
 }
 
+template < typename T, typename Q >
 void Tests< T, Q > :: createBigGraph() {
-    
+    char character;
+    Vertex< T, Q > *origin;
+    Vertex< T, Q > *destiny;
+    Q randomWeightNumber;
+    int randomAristaNumber;
+    graph = new Graph< T, Q >();
+    graph -> create();
+    srand(time(NULL));
+    graphAlgoritms = new GenericGraphAlgorithms< T, Q >(graph);
+    for(int i = 0; i < 125; ++i) {
+        character = i;
+        graph -> addVertex(character);
+    }
+    for(int i = 0; i < 20000; ++i) {
+        randomAristaNumber = rand() % 125;
+        randomWeightNumber = rand() % 101;
+        if(i < 125) {
+            origin = graph -> getVertexByNumber(i);
+            destiny = graph -> getVertexByNumber(randomAristaNumber);
+            if(!graph -> arista(origin, destiny) && randomAristaNumber != i)
+                graph -> addArista(origin, destiny, randomWeightNumber);
+        }
+    }
+    cout << "Vertices: " << graph -> getVertexNumber();
 }
 
+template < typename T, typename Q >
 void Tests< T, Q > :: createSmallGraph() {
-    
+    char character;
+    Vertex< T, Q > *origin;
+    Vertex< T, Q > *destiny;
+    Q randomWeightNumber;
+    int randomAristaNumber;
+    graph = new Graph< T, Q >();
+    graph -> create();
+    srand(time(NULL));
+    graphAlgoritms = new GenericGraphAlgorithms< T, Q >(graph);
+    for(int i = 0; i < 10; ++i) {
+        character = i;
+        graph -> addVertex(character);
+    }
+    for(int i = 0; i < 10; ++i) {
+        randomAristaNumber = rand() % 10;
+        randomWeightNumber = rand() % 101;
+        if(i < 10) {
+            origin = graph -> getVertexByNumber(i);
+            destiny = graph -> getVertexByNumber(randomAristaNumber);
+            if(!graph -> arista(origin, destiny) && randomAristaNumber != i)
+                graph -> addArista(origin, destiny, randomWeightNumber);
+        }
+    }
+    cout << "Vertices: " << graph -> getVertexNumber();
 }
 
-void Tests< T, Q > :: createMiddleGraph() {
-    
+template < typename T, typename Q >
+void Tests< T, Q > :: createMediumGraph() {
+    char character;
+    Vertex< T, Q > *origin;
+    Vertex< T, Q > *destiny;
+    Q randomWeightNumber;
+    int randomAristaNumber;
+    graph = new Graph< T, Q >();
+    graph -> create();
+    srand(time(NULL));
+    graphAlgoritms = new GenericGraphAlgorithms< T, Q >(graph);
+    for(int i = 0; i < 75; ++i) {
+        character = i;
+        graph -> addVertex(character);
+    }
+    for(int i = 0; i < 75; ++i) {
+        randomAristaNumber = rand() % 75;
+        randomWeightNumber = rand() % 101;
+        if(i < 75) {
+            origin = graph -> getVertexByNumber(i);
+            destiny = graph -> getVertexByNumber(randomAristaNumber);
+            if(!graph -> arista(origin, destiny) && randomAristaNumber != i)
+                graph -> addArista(origin, destiny, randomWeightNumber);
+        }
+    }
+    cout << "Vertices: " << graph -> getVertexNumber();
 }
 
 #endif //TESTS_H
